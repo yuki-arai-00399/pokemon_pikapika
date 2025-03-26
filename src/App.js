74a5cecd221ba0362aad7611994ea998210ccec0
yuki19940903 <yuki-arai@cresco-jcube.co.jp>
2025-03-26 18:52:06 +0900

    add pikachu.jpg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './pikachu.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Pika Pika!</h1>
       </header>
     </div>
   );
